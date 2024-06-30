336e9e22202276b671eb7dd55ecc46c6261f8aba
User <user@Users-MacBook-Pro.local>
2024-06-30 17:33:09 -0600

    Hello world

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello promptgrammApp!'
+    return 'Hello promptgrammApp!Howdy'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
