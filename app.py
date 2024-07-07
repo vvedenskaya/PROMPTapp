a065b721fdde4349d38cb295c4e8b6ac169b3bcb
User <user@Users-MacBook-Pro.local>
2024-07-06 18:16:19 -0600

    Add all files

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello promptgrammApp!Howdy'
+def hello():
+    return "Hello, Promptapp!"
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
