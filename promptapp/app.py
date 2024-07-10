21f71bb0b92b9e9e0de1b4e72ef36f771918fc56
Julia <juliavved@gmail.com>
2024-07-09 20:22:49 -0600

    Make updates

---
+++ promptapp/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello, Promptapp!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
